7403b924a4eb3c63bbd765068cff56397e723717
Pongsathorn 'Mick' Panyanithisakul <P.Panyanithisakul@gmail.com>
2014-11-22 16:18:21 +0700

    Commit b4 pulling Champ's code

--- mainapp/apps/trading/auction.py
+++ mainapp/apps/trading/auction.py
@@ -40,11 +40,93 @@ def bid(request) :
 	formstate = request.POST
 
 	request_product = formstate.get('product')
+	requester_list = request.session.get('member')
+	requester_list = models.Member.objects.filter(id=requester)
 	product_list = models.Product.objects.filter(id=request_product)
 
-	if product_list :
+	# auction_new = models.Auction.objects.filter(id)
+
+
+
+
+	if product_list:
 		product = product_list[0]
+		requester = requester_list[0]
 		auction = product.highest_auction
+
+		if formstate.amount != 0 :# the bid request is in manual mode
+			if not auction.isAuto :# The current highest bidder is manual bidding
+				if formstate.amount > auction.current : # challenger wins
+					auction.bidder = requester
+					# auction.ceiling
+					# auction.increase
+					auction.current = formstate.increase
+					auction.isAuto = False
+					# auction.notify
+					# auction.lastbid
+
+				elif formstate.amount <= auction.current : # old bidder wins
+					# do nothing
+					pass
+			else auction.isAuto : # The current highest bidder is auto bidding
+				if formstate.amount > auction.ceiling : # challenger wins
+					auction.bidder = requester
+					# auction.ceiling
+					# auction.increase
+					auction.current = formstate.increase
+					auction.isAuto = False
+					# auction.notify
+					# auction.lastbid
+				elif formstate.amount <= auction.ceiling : # old bidder wins
+					while auction.current <= formstate.amount:
+						auction.current += auction.increase
+						pass
+
+		elif formstate.amount == 0 # the bid request is in auto mode
+			if not auction.isAuto # The current highest bidder is manual bidding
+				if formstate.ceiling > auction.current : # challenger wins
+					auction.bidder = requester
+					auction.ceiling = formstate.ceiling
+					auction.increase = formstate.increase
+					auction.current += formstate.increase
+					auction.isAuto = True
+					# auction.notify
+					# auction.lastbid
+				elif formstate.ceiling <= auction.current : # old bidder wins
+					# do nothing
+					pass
+			else auction.isAuto # The current highest bidder is auto bidding
+				# Fight to determine winner
+				bidder_turn = 0 # 0 = challenger, 1 =  old bidder
+				highest_bidder = auction.bidder
+				highest_ceiling = auction.ceiling
+				highest_increase = auction.increase
+				highest_current = auction.current
+				while auction.ceiling > highest_current and formstate.ceiling > highest_current:
+					if bidder_turn == 0 :
+						highest_bidder = requester
+						highest_ceiling = formstate.ceiling
+						highest_increase = formstate.increase
+						highest_current += formstate.increase
+						# auction.isAuto
+						# auction.notify
+						# auction.lastbid
+						bidder_turn = 1
+					else :
+						highest_bidder = auction.bidder
+						highest_ceiling = auction.ceiling
+						highest_increase = auction.increase
+						highest_current += auction.increase
+						# auction.isAuto
+						# auction.notify
+						# auction.lastbid
+						bidder_turn = 0
+					pass
+		
+				auction.bidder = highest_bidder
+				auction.ceiling = highest_ceiling
+				auction.increase = highest_increase
+				auction.current = highest_current
 
 
 	if formstate.get('')
