d4d6fac3da4cd7807bd075baf15904e743ea1a0e
pakawinz <pakawinz@gmail.com>
2014-11-23 20:46:34 +0700

    finished editprofile

--- mainapp/apps/member/register.py
+++ mainapp/apps/member/register.py
@@ -12,36 +12,62 @@ def main(request):
 	}
 	return context
 
+@common.gen_view('Profile Update','member/register.html',memberOnly=True)
+def edit(request):
+	context = {
+		'member': common.getLoginMember(request),
+		'countries': models.Country.objects.all(),
+		'timezones': models.Timezone.objects.all(),
+	}
+	return context
+
 @common.gen_view(guestOnly=True,postOnly=True,redirect=True)
 def register(request):
 
-	formstate = request.POST
-	email = formstate.get('email')
-	password = formstate.get('password')
-	firstname = formstate.get('firstname')
-	lastname = formstate.get('lastname')
-	displayname = formstate.get('displayname')
-	birthdate = formstate.get('birthdate')
-	phone = formstate.get('phone')
-	address = formstate.get('address')
-	country = formstate.get('country')
-	timezone = formstate.get('timezone')
-	picture = request.FILES.get('picture')
-	submit = formstate.get('isSubmit')
+	result = clean(request, True)
+	if result != True :
+		return result
 
-	password_confirmation = formstate.get('password_confirmation')
-	agreement = formstate.get('agreement')
+	context = { 'content':
+		"Successfully Registered : You should click the link in your email to confirm, blah blah blah"}
+	return render(request,'common/success.html',context)
+
+@common.gen_view(memberOnly=True,postOnly=True,redirect=True)
+def update(request):
+
+	result = clean(request, False)
+	if result != True :
+		return result
+
+	context = { 'content':
+		"Successfully Updated : ei ei ei"}
+	return render(request,'common/success.html',context)
+
+def clean(request,isRegister):
+	email = request.POST.get('email')
+	password = request.POST.get('password')
+	firstname = request.POST.get('firstname')
+	lastname = request.POST.get('lastname')
+	displayname = request.POST.get('displayname')
+	birthdate = request.POST.get('birthdate')
+	phone = request.POST.get('phone')
+	address = request.POST.get('address')
+	country = request.POST.get('country')
+	timezone = request.POST.get('timezone')
+	picture = request.FILES.get('picture')
+
+	oldpassword = request.POST.get('oldpassword')
+	password_confirmation = request.POST.get('password_confirmation')
+	agreement = request.POST.get('agreement')
 
 	errorList = {}
 
-	if not agreement :
-		errorList['agreement'] = ['You should agree to the Terms and Conditions']
 	if password_confirmation != password :
 		errorList['password_confirmation'] = ['Confirmation Password Not Match']
 	try : 
 		birthdate = datetime.datetime.strptime(birthdate,"%d %b %Y")
 	except : 
-		birthdate = datetime.datetime.now()
+		birthdate = datetime.datetime.now().replace(year=2000)
 		errorList['birthdate'] = ['This Birthdate is not valid format (should be like 11 Dec 2001)']
 	try : 
 		country=models.Country.objects.get(id=country)
@@ -54,17 +80,37 @@ def register(request):
 		timezone=models.Timezone.objects.first()
 		errorList['timezone'] = ['This Timezone is not in Database']
 
-	confirm = hashlib.sha224(email+password).hexdigest()
-
-	member = models.Member(
-		email=email,password=password,
-		firstname=firstname,lastname=lastname,
-		displayname=displayname,birthdate=birthdate,
-		phone=phone,address=address,
-		country=country,timezone=timezone,
-		picture=picture,confirmation=confirm)
-
 	context = {}
+
+	if isRegister :
+		if not agreement :
+			errorList['agreement'] = ['You should agree to the Terms and Conditions']
+
+		confirm = hashlib.sha224(email+password).hexdigest()
+
+		member = models.Member(
+			email=email,password=password,
+			firstname=firstname,lastname=lastname,
+			displayname=displayname,birthdate=birthdate,
+			phone=phone,address=address,
+			country=country,timezone=timezone,
+			picture=picture,confirmation=confirm)
+	else :
+		member = common.getLoginMember(request)
+
+		if password : 
+			if oldpassword != member.password :
+				errorList['oldpassword'] = ['Old Password Not Match']
+			member.password = password
+		if firstname : member.firstname = firstname
+		if lastname : member.lastname = lastname
+		if displayname : member.displayname = displayname
+		if birthdate : member.birthdate = birthdate
+		if phone : member.phone = phone
+		if address : member.address = address
+		if country : member.country = country
+		if timezone : member.timezone = timezone
+		if picture : member.picture = picture
 
 	try :
 		member.full_clean()
@@ -76,8 +122,7 @@ def register(request):
 				for exception in exceptions :
 					errorList[field].append(unicode(exception.message))
 
-
-	if not submit :
+	if not request.POST.get('isSubmit') :
 		if errorList :
 			context['errorList'] = errorList
 		else :
@@ -94,8 +139,9 @@ def register(request):
 		return render(request,'common/invalid.html',context)
 
 	member.save()
-	confirm_url = "http://127.0.0.1:8000/member/confirm?email=" + email + "&confirm=" + confirm
-	common.sendmail("Aucport : Email Confirmation", "Please Click This Link : " + confirm_url, [email])
-	context = { 'content':
-		"Successfully Registered : You should click the link in your email to confirm, blah blah blah"}
-	return render(request,'common/success.html',context)
+
+	if isRegister :
+		confirm_url = "http://127.0.0.1:8000/member/confirm?email=" + email + "&confirm=" + confirm
+		common.sendmail("Aucport : Email Confirmation", "Please Click This Link : " + confirm_url, [email])
+
+	return True
