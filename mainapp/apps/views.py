0f8e657667c2621f2c7974c66d27476c2996249c
pakawinz <pakawinz@gmail.com>
2014-11-26 05:58:33 +0700

    do more css

--- mainapp/apps/views.py
+++ mainapp/apps/views.py
@@ -5,6 +5,7 @@ from mainapp import common, models
 def home(request) :
 	context = {
 		'products':models.Product.objects.filter(
-			state__in=[models.Product.STATE_AUCTION,models.Product.STATE_SELLING]),
+			state__in=[models.Product.STATE_AUCTION,models.Product.STATE_SELLING]).order_by('-view'),
+		'randomdot':range(36),
 	}
 	return context
